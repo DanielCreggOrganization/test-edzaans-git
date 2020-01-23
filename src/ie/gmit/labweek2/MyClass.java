b49a8a5b605f1919701da2509347b905fc4d0dc1
edzaans-git <g00377012@gmit.ie>
2020-01-23 14:25:41 +0000

    change2

---
+++ src/ie/gmit/labweek2/MyClass.java
@@ -0,0 +1,11 @@
+package ie.gmit.labweek2;
+
+class MyClass {
+    public static void main(String[] args) {
+        myMethod();
+    }
+
+    public static void myMethod() {
+        System.out.println("I just got executed");
+    }
+}
