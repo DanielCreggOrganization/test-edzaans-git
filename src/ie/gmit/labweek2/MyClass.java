fe1286c71ea8ee7d1ce567e1557e7401dcba0931
edzaans-git <g00377012@gmit.ie>
2020-01-23 14:34:29 +0000

    2methods

--- src/ie/gmit/labweek2/MyClass.java
+++ src/ie/gmit/labweek2/MyClass.java
@@ -4,11 +4,13 @@ class MyClass {
     public static void main(String[] args) {
         for (int i = 0; i < 7; i++) {
             myMethod();
+            myMethod2();
         }
-
     }
 
     public static void myMethod() {
         System.out.println("I just got executed");
     }
+     public static void myMethod2() {
+        System.out.println("I just got executed again");
 }
